60dabc756ab0a46509d7fae0737b550c26af0633
AnkushSarkar10 <ankush.sarkar2002@gmail.com>
2024-01-13 13:58:30 -0500

    yuh

---
+++ frontend/components/SideBar.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+</script>
+
+<template>
+    <div class="w-1/3 border h-[90vh]">
+        side barr
+    </div>
+</template>
+
+<style scoped>
+</style>
---
+++ frontend/components/Task/card.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+</script>
+
+<template>
+    <div class="bg-[#778F77] m-1 p-2 rounded-md">
+        cardd
+    </div>
+</template>
+
+<style scoped>
+</style>
