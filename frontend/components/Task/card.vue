d44440ec114015bab72ec2c7a3abe4f9c0718475
AnkushSarkar10 <ankush.sarkar2002@gmail.com>
2024-01-13 14:28:10 -0500

    yuh

--- frontend/components/Task/card.vue
+++ frontend/components/Task/card.vue
@@ -2,8 +2,15 @@
 </script>
 
 <template>
-    <div class="bg-[#778F77] m-1 p-2 rounded-md">
-        cardd
+    <div class="flex  bg-[#778F77] m-1 p-2 rounded-md text-black">
+        <div class="flex-col">
+            <h1 class="font-bold text-2xl">Task Name</h1>
+            <h2 class="pl-5">Urgency: High</h2>
+            <h2 class="pl-5">Compatibility: 50%</h2>
+        </div>
+        <div class="self">
+            <Icon name="ph:dots-three-outline-fill" color="black" />
+        </div>
     </div>
 </template>
 
