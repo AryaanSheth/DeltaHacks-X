aa6e5a4379e3e3f2ae59694f7a7b70c83953ac99
taniadasilva17 <46635552+taniadasilva17@users.noreply.github.com>
2024-01-13 22:02:02 -0500

    :3 ? idk :P

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <div class="w-full h-10 text-left border border-[#4C503F] bg-[#35382C] d-flex">
+    <div class="w-full h-10 text-left border border-[#61554e] bg-[#665b53] d-flex">
         <img src="~/assets/images/tiger.png" width="40">
         <img src="~/assets/images/taiket.png" width="80">
     </div>
