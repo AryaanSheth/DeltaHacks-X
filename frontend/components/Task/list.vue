decd0748eb9d7d46189f073c5e5310eb9c17c3db
taniadasilva17 <46635552+taniadasilva17@users.noreply.github.com>
2024-01-14 10:19:49 -0500

    tasks :3

--- frontend/components/Task/list.vue
+++ frontend/components/Task/list.vue
@@ -6,10 +6,10 @@ const isOpen = ref(false)
 
 <template>
     <div class="flex flex-col p-3 h-fit bg-[#5B6E5B] rounded-lg">
-        <TaskCard taskName="name" taskDescription="taskDescription" :urgency="2" :compatibility="0.5"/>
-        <TaskCard taskName="name" taskDescription="taskDescription" :urgency="1" :compatibility="0.5"/>
-        <TaskCard taskName="name" taskDescription="taskDescription" :urgency="1" :compatibility="0.1"/>
-        <TaskCard taskName="name" taskDescription="taskDescription" :urgency="1" :compatibility="0.5"/>
+        <TaskCard taskName="name1" taskDescription="taskDescription1" :urgency="2" :compatibility="0.80"/>
+        <TaskCard taskName="name3" taskDescription="taskDescription3" :urgency="1" :compatibility="0.75"/>
+        <TaskCard taskName="name2" taskDescription="taskDescription2" :urgency="1" :compatibility="0.5"/>
+        <TaskCard taskName="name4" taskDescription="taskDescription4" :urgency="1" :compatibility="0.1"/>
 
         <div class="self-center" v-if="isAdmin">
             <button class="add-task-button rounded-lg text-center py-3 px-20 mt-5 mb-2 bg-[#485B49] text-[#F1F1E9]" @click="isOpen = true">Add Task</button>
