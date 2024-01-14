ab77999d56cc74ee04c8ad0408e564b355c50e9f
taniadasilva17 <46635552+taniadasilva17@users.noreply.github.com>
2024-01-13 22:50:16 -0500

    :P cute quick guide + going to sleep :3

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -1,12 +1,25 @@
 <script setup lang='ts'>
+const isOpen = ref(false)
 </script>
 
 <template>
-    <div class="w-full h-10 text-left border border-[#61554e] bg-[#665b53]">
-        <NuxtLink to="/" class="flex">
-            <img src="~/assets/images/tiger.png" width="40">
-            <img src="~/assets/images/taiket.png" width="80">
-        </NuxtLink>
+    <div class="w-full h-15 text-left border border-[#61554e] bg-[#665b53] flex justify-between items-center">
+        <div class="flex">
+            <NuxtLink to="/">
+                <img src="~/assets/images/tiger.png" width="40">
+            </NuxtLink>
+            <NuxtLink to="/">
+                <img src="~/assets/images/taiket.png" width="80">
+            </NuxtLink>
+        </div>
+        <div>
+        <button class="elipses text-2xl" @click="isOpen = true">
+            <Icon name="ph:question" color="#F1F1E9" />
+        </button>
+        <UModal v-model="isOpen">
+                <TaskHelp/>
+            </UModal>
+        </div>
     </div>
 </template>
 
