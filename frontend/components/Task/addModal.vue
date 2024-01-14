36886c3a36abaef575dcbd274b7f290876b8ccc9
taniadasilva17 <46635552+taniadasilva17@users.noreply.github.com>
2024-01-14 09:05:20 -0500

    bleh 2.0 :p

--- frontend/components/Task/addModal.vue
+++ frontend/components/Task/addModal.vue
@@ -11,6 +11,12 @@
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
                     placeholder="" required>
             </div>
+            <div class="mb-5">
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Task Description</label>
+                <input type="description" id="name"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+                    placeholder="" required>
+            </div>
             <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an
                 option</label>
             <select id="countries"
