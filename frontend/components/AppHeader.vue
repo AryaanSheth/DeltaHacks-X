07f987e5637fef3e399fd3c24d4c8375cc3dc93b
AnkushSarkar10 <ankush.sarkar2002@gmail.com>
2024-01-13 21:30:35 -0500

    yuh

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -2,14 +2,13 @@
 </script>
 
 <template>
-    <div class="w-full h-10 text-left border border-[#4C503F] bg-[#35382C] d-flex">
-        <img src="~/assets/images/tiger.png" width="40">
-        <img src="~/assets/images/taiket.png" width="80">
+    <div class="w-full h-10 text-left border border-[#4C503F] bg-[#35382C]">
+        <NuxtLink to="/" class="flex">
+            <img src="~/assets/images/tiger.png" width="40">
+            <img src="~/assets/images/taiket.png" width="80">
+        </NuxtLink>
     </div>
 </template>
 
 <style scoped>
-.d-flex {
-    display: flex;
-}
 </style>
--- frontend/pages/invite.vue
+++ frontend/pages/invite.vue
@@ -28,13 +28,13 @@ async function onSubmit(event: FormSubmitEvent<any>) {
             <div class="mb-5">
                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
                 <input type="name" id="name"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
                     placeholder="" required>
             </div>
             <div class="mb-5">
                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                 <input type="email" id="email"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
                     required>
             </div>
 
